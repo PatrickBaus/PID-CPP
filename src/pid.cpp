PatrickBaus/PID-CPP

commit 00f91de31868cb900020c4f8817ab72ba11c6958

Note: Breaking change. The feedack direction contant in pid.h has changed. feedbackNegative now means, that the plant will reduce the error on a positive signal. If using numeric constants, nothing changes. This change makes the naming more consistent. Also fixed a few typos.

src/pid.cpp
@@ -38,7 +38,7 @@ namespace Pid {
       @param input The new input variable in Q32.0 format, that is with zero frational precision. The value is
         an unsigned integer in Offset Binary notation.
       @param *output The output in Q(DAC_resolution).0 format and is determinded by the ki, kp, kd
-        parameters used in the multiplications. The encoding is Offset Binary, so it directly be fed to most DACs.
+        parameters used in the multiplications. The encoding is Offset Binary, so it can be directly fed to most DACs.
   */
   const uint32_t PID::compute(const uint32_t input) {
       // Calcualte P term
@@ -54,7 +54,7 @@ namespace Pid {
           this->ki,
           error
       );
-      // Calculate D term (Note: We actually calcualte -dInput)
+      // Calculate D term (Note: We actually calculate -dInput)
       // We do not calculate dError, because this would cause an output spike every time someone changes the setpoint
       // dError = d(Setpoint - Input)_n - d(Setpoint - Input)_(n-1)
       //        = dSetpoint - dInput
@@ -99,9 +99,10 @@ namespace Pid {
   /** Note: ki and kd must be normalized to the sampling time
    */
   void PID::setTunings(const int32_t kp, const int32_t ki, const int32_t kd, const ProportionalGain proportionalGain) {
-      this->kp = (this->feedbackDirection == feedbackPositive) ? abs(kp) : -abs(kp);
-      this->ki = (this->feedbackDirection == feedbackPositive) ? abs(ki) : -abs(ki);
-      this->kd = (this->feedbackDirection == feedbackPositive) ? abs(kd) : -abs(kd);
+      // Invert the output, if the plant response to an error is positive
+      this->kp = (this->feedbackDirection == feedbackNegative) ? abs(kp) : -abs(kp);
+      this->ki = (this->feedbackDirection == feedbackNegative) ? abs(ki) : -abs(ki);
+      this->kd = (this->feedbackDirection == feedbackNegative) ? abs(kd) : -abs(kd);
       
       this->proportionalGain = proportionalGain;
   }

src/pid.h
@@ -96,8 +96,8 @@ namespace Pid {
   }
 
   enum FeedbackDirection {
-      feedbackPositive = 0,
-      feedbackNegative = 1,
+      feedbackNegative = 0,
+      feedbackPositive = 1,
   };
 
   enum ProportionalGain {

commit 402ac969f0ad361ae5890e9a05adc77c9e9f995b

Refactoring Added getter to extract the PID integrator error Added init(initialInput, initialErrorSum) to initialize all internal state Fixed incorrect PID direction setting. The bug was introduced in 00f91de31868cb900020c4f8817ab72ba11c6958

src/pid.cpp
@@ -40,7 +40,7 @@ namespace Pid {
       @param *output The output in Q(DAC_resolution).0 format and is determinded by the ki, kp, kd
         parameters used in the multiplications. The encoding is Offset Binary, so it can be directly fed to most DACs.
   */
-  const uint32_t PID::compute(const uint32_t input) {
+  uint32_t PID::compute(const uint32_t input) {
       // Calcualte P term
       // Note: the calculation is (uint32_t)setpoint - (uint32_t)(input) = (int32_t)error (using signed math)
       // This is true for offset binary values!
@@ -99,11 +99,13 @@ namespace Pid {
   /** Note: ki and kd must be normalized to the sampling time
    */
   void PID::setTunings(const int32_t kp, const int32_t ki, const int32_t kd, const ProportionalGain proportionalGain) {
-      // Invert the output, if the plant response to an error is positive
-      this->kp = (this->feedbackDirection == feedbackNegative) ? abs(kp) : -abs(kp);
-      this->ki = (this->feedbackDirection == feedbackNegative) ? abs(ki) : -abs(ki);
-      this->kd = (this->feedbackDirection == feedbackNegative) ? abs(kd) : -abs(kd);
-      
+      // We calculate error as: error = setpoint - input
+      // If the error is positive, the control output will be positive as well,
+      // so we need to invert the output if the plant response to an error is negative
+      this->kp = (this->feedbackDirection == feedbackPositive) ? abs(kp) : -abs(kp);
+      this->ki = (this->feedbackDirection == feedbackPositive) ? abs(ki) : -abs(ki);
+      this->kd = (this->feedbackDirection == feedbackPositive) ? abs(kd) : -abs(kd);
+
       this->proportionalGain = proportionalGain;
   }
 
@@ -123,12 +125,21 @@ namespace Pid {
       this->setpoint = value;
   }
 
-  const uint32_t PID::getSetpoint() {
+  uint32_t PID::getSetpoint() {
       return this->setpoint;
   }
 
+  int32_t PID::getIntegratorError() {
+      return this->errorSum;
+  }
+
   void PID::init(const uint32_t initialInput) {
-     this->previousInput = initialInput;
+      PID::init(initialInput, 0);
+  }
+
+  void PID::init(const uint32_t initialInput, const int32_t initialErrorSum) {
+      this->previousInput = initialInput;
+      this->errorSum = clamp(initialErrorSum, this->outputMin, this->outputMax);
   }
 
   void PID::updateOutput(const uint32_t value) {
@@ -141,15 +152,15 @@ namespace Pid {
     this->setTunings(this->kp, this->ki, this->kd);
   }
 
-  const uint32_t PID::getKp() {
+  uint32_t PID::getKp() {
       return this->kp;
   }
 
-  const uint32_t PID::getKi() {
+  uint32_t PID::getKi() {
       return  this->ki;
   }
 
-  const uint32_t PID::getKd() {
+  uint32_t PID::getKd() {
       return this->kd;
   }
 }   // namespace Pid

src/pid.h
@@ -1,7 +1,7 @@
 #ifndef PID_H
 #define PID_H
 
-#include <stdint.h>
+#include <stdint.h>    // uint8_t, etc.
 
 #define LIKELY(x)       __builtin_expect(!!(x), 1)
 #define UNLIKELY(x)     __builtin_expect(!!(x), 0)
@@ -111,20 +111,22 @@ namespace Pid {
           PID(const uint32_t setpoint, const int32_t kp, const int32_t ki, const int32_t kd, uint8_t _qn, FeedbackDirection feedbackDirection);
           PID(const uint32_t setpoint, const int32_t kp, const int32_t ki, const int32_t kd, uint8_t _qn);
 
-          const uint32_t compute(uint32_t input);
+          uint32_t compute(uint32_t input);
 
           void setTunings(const int32_t kp, const int32_t ki, const int32_t kd);
           void setTunings(const int32_t kp, const int32_t ki, const int32_t kd, const ProportionalGain proportionalGain);
-          const uint32_t getKp();
-          const uint32_t getKi();
-          const uint32_t getKd();
+          uint32_t getKp();
+          uint32_t getKi();
+          uint32_t getKd();
           void setSetpoint(const uint32_t value);
-          const uint32_t getSetpoint();
+          uint32_t getSetpoint();
+          int32_t getIntegratorError();
           void setControllerFeedback(const FeedbackDirection feedbackDirection);
           void setOutputMin(const uint32_t value);
           void setOutputMax(const uint32_t value);
           void updateOutput(const uint32_t value);
           void init(const uint32_t initialInput);
+          void init(const uint32_t initialInput, const int32_t initialErrorSum);
       private:
           int32_t kp, ki, kd;
           FeedbackDirection feedbackDirection;